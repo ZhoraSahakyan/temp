e0074c5c7ef1949dbbcdfc21ff7c829118bacaab
Zhora Sahakyan <zhora.sahakyan@synisys.com>
2024-08-01 14:11:18 +0400

    chore: Enable authorization guard for admin panel route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('./pages/admin-panel/admin-panel.module').then(m => m.AdminPanelModule),
-    // canActivate: [AuthorizeGuard]
+    canActivate: [AuthorizeGuard]
   },
   {
     path: '**',
